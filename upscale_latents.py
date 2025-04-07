9b3e8235006b8b18a15ed4727e15bb5d73c12e4d
gogurtenjoyer <36354352+gogurtenjoyer@users.noreply.github.com>
2025-04-07 15:58:19 -0400

    Update to use Invoke's new Model Load API
    Rather than using hf_hub_download or a direct load, we're now using Invoke's API for this. Shouldn't be a big change since these models are small, but it lets Invoke know what's going on, which may be helpful. Can still load from the local models folder, too.

--- upscale_latents.py
+++ upscale_latents.py
@@ -1,10 +1,10 @@
 import os
+from pathlib import Path
 from typing import Literal
 
 import torch
 import torch.nn as nn
 from safetensors.torch import load_file
-from huggingface_hub import hf_hub_download
 
 from invokeai.app.invocations.fields import FieldDescriptions
 
@@ -73,7 +73,7 @@ FACTORS = Literal[('1.25', '1.5', '2.0')]
     title="Upscale Latents",
     tags=["latents", "upscale"],
     category="latents",
-    version="1.1.0",
+    version="1.5.0",
 )
 class UpscaleLatentsInvocation(BaseInvocation):
     """Upscales latents using a trained model"""
@@ -86,31 +86,34 @@ class UpscaleLatentsInvocation(BaseInvocation):
     scale_factor: FACTORS = InputField(default='2.0', input=Input.Direct)
     magic_number: float = InputField(default=0.18215, description="Unless you have a great reason, do not change from 0.18215!")
 
+
     def invoke(self, context: InvocationContext) -> LatentsOutput:
         latents = context.tensors.load(self.latents.latents_name)
 
         latents = torch.div(latents, self.magic_number) # MAGIC NUMBER! YOU'LL SEE ME AGAIN!!
 
-        model = Upscaler(self.scale_factor)
+        model = Upscaler(float(self.scale_factor))
 
         filename = f"latent-upscaler-v{model.version}_SD{self.latent_ver}-x{self.scale_factor}.safetensors"
         local = os.path.join(
             os.path.join(os.path.dirname(os.path.realpath(__file__)),"models"),
             filename
         )
+        
+        model_path = Path(local) if os.path.isfile(local) else None
 
-        if os.path.isfile(local):
+        if model_path and model_path.exists():
             context.logger.info("[Upscale Latents] Using Local Model")
-            weights = local
+            loaded_model = context.models.load_local_model(model_path)
         else:
             context.logger.info("[Upscale Latents] Using HF Hub Model")
-            weights = str(hf_hub_download(
-                repo_id="city96/SD-Latent-Upscaler",
-                filename=filename)
+            # Download and cache the model, then load it
+            loaded_model = context.models.load_remote_model(
+                # source=f"city96/SD-Latent-Upscaler/{filename}"  ME NO WORK, NO USE ME
+                source=f"https://huggingface.co/city96/SD-Latent-Upscaler/resolve/main/{filename}?download=true"
             )
 
-
-        model.load_state_dict(load_file(weights))
+        model.load_state_dict(loaded_model.model)
         lt = latents.to("cpu", torch.float32)
         resized_latents = model(lt)
         resized_latents = torch.mul(resized_latents, self.magic_number) # HELLO AGAIN FROM YOUR OLD FRIEND, MAGIC NUMBER!
